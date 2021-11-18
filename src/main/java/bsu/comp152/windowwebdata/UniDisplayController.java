dafbd94a56371708399e2a3fcc2c255db9573fea
LosSantosSavior <nickrussell001@gmail.com>
2021-11-18 09:58:33 -0500

    commit 11/18/21 9:59AM

--- src/main/java/bsu/comp152/windowwebdata/UniDisplayController.java
+++ src/main/java/bsu/comp152/windowwebdata/UniDisplayController.java
@@ -1,5 +1,7 @@
 package bsu.comp152.windowwebdata;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -18,7 +20,7 @@ public class UniDisplayController implements Initializable {
     @FXML
     private TextField UniversityCountryField;
     @FXML
-    private TextField universityDisplayField;
+    private TextField websiteDisplayField;
     @FXML
     private ListView<DataHandler.universityDataType> ListControl;
     private DataHandler Model;
@@ -48,5 +50,13 @@ public class UniDisplayController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         loadData();
+        ListControl.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<DataHandler.universityDataType>() {
+            @Override
+            public void changed(ObservableValue<? extends DataHandler.universityDataType> observableValue, DataHandler.universityDataType universityDataType, DataHandler.universityDataType t1) {
+                NameField.setText(t1.name);
+                UniversityCountryField.setText(t1.country);
+                websiteDisplayField.setText(t1.web_pages.toString());
+            }
+        });
     }
 }
